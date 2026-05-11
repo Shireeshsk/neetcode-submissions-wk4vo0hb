9050a9f5054d67000d67af5b2e16f9c9f64cce54
Shireeshsk <166245115+Shireeshsk@users.noreply.github.com>
2026-05-11 20:12:51 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int f = 0;
+        int t = 0;
+        for(int i : bills){
+            if(i==5) f++;
+            else if(i==10){
+                if(f<=0) return false;
+                t++;
+                f--;
+            }
+            else{
+                if(t<=0){
+                    if(f<3) return false;
+                    f -=3;
+                }
+                else{
+                    if(f<1) return false;
+                    f--;
+                    t--;
+                }
+            }
+        }
+        return true;
+    }
+}
