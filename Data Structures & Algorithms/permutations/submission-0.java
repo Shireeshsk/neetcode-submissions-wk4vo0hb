79435e4386f53be6ce67d1cac8b731d24edcec94
Shireeshsk <166245115+Shireeshsk@users.noreply.github.com>
2026-06-11 20:22:26 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean vis[] = new boolean[arr.length];
+        helper(0,0,arr,new ArrayList<>(),res,vis);
+        return res;
+    }
+
+    public void helper(int idx,int s,int arr[],List<Integer> list,List<List<Integer>> res , boolean vis[]){
+        if(s>=arr.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i = 0;i<arr.length;i++){
+            if(vis[i]) continue;
+            vis[i] = true;
+            list.add(arr[i]);
+            helper(i+1,s+1,arr,list,res,vis);
+            list.remove(list.size()-1);
+            vis[i] = false;
+        }
+    }
+}
