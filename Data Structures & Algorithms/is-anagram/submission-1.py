761b6135e2922aade002afbf72f12b8a9b577b31
Shireeshsk <166245115+Shireeshsk@users.noreply.github.com>
2026-07-06 19:33:34 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        arr = [0]*26
+        for i in s:
+            arr[ord(i)-ord('a')] += 1
+        for i in t:
+            arr[ord(i)-ord('a')]-= 1
+        for i in arr:
+            if i!=0:
+                return False
+        return True
+        
